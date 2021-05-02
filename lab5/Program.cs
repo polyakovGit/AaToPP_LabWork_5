52adacc62454942c06386ecd92ef97497a74c697
BuildTools <unconfigured@null.spigotmc.org>
2021-05-02 11:16:25 +0500

    добавил параметризированные потоки

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -6,46 +6,62 @@ namespace Lab3
 {
     class Program
     {
-        static string buffer;
+
+        class Message
+        {
+            int n;
+            bool bEmpty;
+            bool finish;
+            string buffer;
+            public Message(int n, bool bEmpty, bool finish)
+            {
+                this.n = n;
+                this.bEmpty = bEmpty;
+                this.finish = finish;
+                buffer = null;
+            }
+            public void SetFinish(bool value)
+            {
+                finish = value;
+            }
+            public void Read()
+            {
+                List<string> MyMessagesRead = new List<string>();//локальный массив читателя
+                while (!finish)
+                    if (!bEmpty)
+                    {
+                        MyMessagesRead.Add(buffer);
+                        bEmpty = true;
+                    }
+                //заносим в статический список, чтобы проверить содержимое
+                //       ResultRea.Add(MyMessagesRead);
+            }
+            public void Write()
+            {
+                string[] MyMessagesWri = new string[n];//локальный массив писателя
+                for (int j = 0; j < n; j++)
+                    MyMessagesWri[j] = j.ToString();
+                // MyMessagesWri[j] = "Thread WRI #" + Thread.CurrentThread.Name + ", Message: " + j.ToString();//заменить
+                int i = 0;
+                while (i < n)
+                    if (bEmpty)
+                    {
+                        buffer = MyMessagesWri[i++];
+                        bEmpty = false;
+                    }
+                //заносим в статический список, чтобы проверить содержимое
+                //     ResultWri.Add(MyMessagesWri);
+            }
+        }
 
         //список дял проверки массивов писателей
         //static List<string[]> ResultWri = new List<string[]>();
         //список для проверки массивов читателей
         //static List<List<string>> ResultRea = new List<List<string>>();
-
-        static bool bEmpty = true;
-        static bool finish = false;
-        static int n = 1000000;
-        static void Read()
-        {
-            List<string> MyMessagesRead = new List<string>();//локальный массив читателя
-            while (!finish)
-                if (!bEmpty)
-                {
-                    MyMessagesRead.Add(buffer);
-                    bEmpty = true;
-                }
-            //заносим в статический список, чтобы проверить содержимое
-            //       ResultRea.Add(MyMessagesRead);
-        }
-        static void Write( )
-        {
-            string[] MyMessagesWri = new string[n];//локальный массив писателя
-            for (int j = 0; j < (int)n; j++)
-                MyMessagesWri[j] = j.ToString();
-            // MyMessagesWri[j] = "Thread WRI #" + Thread.CurrentThread.Name + ", Message: " + j.ToString();//заменить
-            int i = 0;
-            while (i < n)
-                if (bEmpty)
-                {
-                    buffer = MyMessagesWri[i++];
-                    bEmpty = false;
-                }
-            //заносим в статический список, чтобы проверить содержимое
-            //     ResultWri.Add(MyMessagesWri);
-        }
+       
         static void Main()
         {
+            Message Serial = new Message(1000000, true, false);
             DateTime dt1, dt2;
             int R = 2; // Параметр N - число писателей
             int W = 2; // Параметр M - число читателей
@@ -54,17 +70,17 @@ namespace Lab3
             dt1 = DateTime.Now;
             for (int i = 0; i < W; i++)
             {
-                Writers[i] = new Thread(Write);
+                Writers[i] = new Thread(new ThreadStart(Serial.Write));
                 Writers[i].Start();
             }
             for (int i = 0; i < R; i++)
             {
-                Readers[i] = new Thread(Read);
+                Readers[i] = new Thread(new ThreadStart(Serial.Read));
                 Readers[i].Start();
             }
             for (int i = 0; i < W; i++)
                 Writers[i].Join();
-            finish = true;//завершаем работу читателей
+            Serial.SetFinish(true);//завершаем работу читателей
             for (int i = 0; i < R; i++)
                 Readers[i].Join();
             dt2 = DateTime.Now;
